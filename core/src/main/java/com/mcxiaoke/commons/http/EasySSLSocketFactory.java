d93b2f521a3e47cfda2b9802e164cc8f9eb06851
zhangxiaoke <zhangxiaoke@douban.com>
2014-02-07 15:35:40 +0800

    remove http request

--- core/src/main/java/com/mcxiaoke/commons/http/EasySSLSocketFactory.java
+++ core/src/main/java/com/mcxiaoke/commons/http/EasySSLSocketFactory.java
@@ -36,7 +36,7 @@ import java.net.UnknownHostException;
 /**
  * This socket factory will create ssl socket that accepts self signed
  * certificate
- * 
+ *
  * @author olamy
  * @version $Id: EasySSLSocketFactory.java 765355 2009-04-15 20:59:07Z evenisse
  *          $
@@ -44,92 +44,92 @@ import java.net.UnknownHostException;
  */
 public class EasySSLSocketFactory implements LayeredSocketFactory {
 
-	private SSLContext sslcontext = null;
+    private SSLContext sslcontext = null;
 
-	private static SSLContext createEasySSLContext() throws IOException {
-		try {
-			SSLContext context = SSLContext.getInstance("TLS");
-			context.init(null, new TrustManager[] { new EasyX509TrustManager(
-					null) }, null);
-			return context;
-		} catch (Exception e) {
-			throw new IOException(e.getMessage());
-		}
-	}
+    private static SSLContext createEasySSLContext() throws IOException {
+        try {
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new TrustManager[]{new EasyX509TrustManager(
+                    null)}, null);
+            return context;
+        } catch (Exception e) {
+            throw new IOException(e.getMessage());
+        }
+    }
 
-	private SSLContext getSSLContext() throws IOException {
-		if (this.sslcontext == null) {
-			this.sslcontext = createEasySSLContext();
-		}
-		return this.sslcontext;
-	}
+    private SSLContext getSSLContext() throws IOException {
+        if (this.sslcontext == null) {
+            this.sslcontext = createEasySSLContext();
+        }
+        return this.sslcontext;
+    }
 
-	/**
-	 * @see org.apache.http.conn.scheme.SocketFactory#connectSocket(java.net.Socket,
-	 *      String, int, java.net.InetAddress, int,
-	 *      org.apache.http.params.HttpParams)
-	 */
-	public Socket connectSocket(Socket sock, String host, int port,
-			InetAddress localAddress, int localPort, HttpParams params)
-			throws IOException, UnknownHostException, ConnectTimeoutException {
-		int connTimeout = HttpConnectionParams.getConnectionTimeout(params);
-		int soTimeout = HttpConnectionParams.getSoTimeout(params);
+    /**
+     * @see org.apache.http.conn.scheme.SocketFactory#connectSocket(java.net.Socket,
+     * String, int, java.net.InetAddress, int,
+     * org.apache.http.params.HttpParams)
+     */
+    public Socket connectSocket(Socket sock, String host, int port,
+                                InetAddress localAddress, int localPort, HttpParams params)
+            throws IOException, UnknownHostException, ConnectTimeoutException {
+        int connTimeout = HttpConnectionParams.getConnectionTimeout(params);
+        int soTimeout = HttpConnectionParams.getSoTimeout(params);
 
-		InetSocketAddress remoteAddress = new InetSocketAddress(host, port);
-		SSLSocket sslsock = (SSLSocket) ((sock != null) ? sock : createSocket());
+        InetSocketAddress remoteAddress = new InetSocketAddress(host, port);
+        SSLSocket sslsock = (SSLSocket) ((sock != null) ? sock : createSocket());
 
-		if ((localAddress != null) || (localPort > 0)) {
-			// we need to bind explicitly
-			if (localPort < 0) {
-				localPort = 0; // indicates "any"
-			}
-			InetSocketAddress isa = new InetSocketAddress(localAddress,
-					localPort);
-			sslsock.bind(isa);
-		}
+        if ((localAddress != null) || (localPort > 0)) {
+            // we need to bind explicitly
+            if (localPort < 0) {
+                localPort = 0; // indicates "any"
+            }
+            InetSocketAddress isa = new InetSocketAddress(localAddress,
+                    localPort);
+            sslsock.bind(isa);
+        }
 
-		sslsock.connect(remoteAddress, connTimeout);
-		sslsock.setSoTimeout(soTimeout);
-		return sslsock;
+        sslsock.connect(remoteAddress, connTimeout);
+        sslsock.setSoTimeout(soTimeout);
+        return sslsock;
 
-	}
+    }
 
-	/**
-	 * @see org.apache.http.conn.scheme.SocketFactory#createSocket()
-	 */
-	public Socket createSocket() throws IOException {
-		return getSSLContext().getSocketFactory().createSocket();
-	}
+    /**
+     * @see org.apache.http.conn.scheme.SocketFactory#createSocket()
+     */
+    public Socket createSocket() throws IOException {
+        return getSSLContext().getSocketFactory().createSocket();
+    }
 
-	/**
-	 * @see org.apache.http.conn.scheme.SocketFactory#isSecure(java.net.Socket)
-	 */
-	public boolean isSecure(Socket socket) throws IllegalArgumentException {
-		return true;
-	}
+    /**
+     * @see org.apache.http.conn.scheme.SocketFactory#isSecure(java.net.Socket)
+     */
+    public boolean isSecure(Socket socket) throws IllegalArgumentException {
+        return true;
+    }
 
-	/**
-	 * @see org.apache.http.conn.scheme.LayeredSocketFactory#createSocket(java.net.Socket,
-	 *      String, int, boolean)
-	 */
-	public Socket createSocket(Socket socket, String host, int port,
-			boolean autoClose) throws IOException, UnknownHostException {
+    /**
+     * @see org.apache.http.conn.scheme.LayeredSocketFactory#createSocket(java.net.Socket,
+     * String, int, boolean)
+     */
+    public Socket createSocket(Socket socket, String host, int port,
+                               boolean autoClose) throws IOException, UnknownHostException {
         return getSSLContext().getSocketFactory().createSocket(socket, host, port, autoClose);
-	}
+    }
 
-	// -------------------------------------------------------------------
-	// javadoc in org.apache.http.conn.scheme.SocketFactory says :
-	// Both Object.equals() and Object.hashCode() must be overridden
-	// for the correct operation of some connection managers
-	// -------------------------------------------------------------------
+    // -------------------------------------------------------------------
+    // javadoc in org.apache.http.conn.scheme.SocketFactory says :
+    // Both Object.equals() and Object.hashCode() must be overridden
+    // for the correct operation of some connection managers
+    // -------------------------------------------------------------------
 
-	public boolean equals(Object obj) {
-		return ((obj != null) && obj.getClass().equals(
-				EasySSLSocketFactory.class));
-	}
+    public boolean equals(Object obj) {
+        return ((obj != null) && obj.getClass().equals(
+                EasySSLSocketFactory.class));
+    }
 
-	public int hashCode() {
-		return EasySSLSocketFactory.class.hashCode();
-	}
+    public int hashCode() {
+        return EasySSLSocketFactory.class.hashCode();
+    }
 
 }
--- core/src/main/java/com/mcxiaoke/commons/http/EasyX509TrustManager.java
+++ core/src/main/java/com/mcxiaoke/commons/http/EasyX509TrustManager.java
@@ -34,58 +34,49 @@ import java.security.cert.X509Certificate;
  * @since 1.2.3
  */
 public class EasyX509TrustManager
-    implements X509TrustManager
-{
+        implements X509TrustManager {
 
     private X509TrustManager standardTrustManager = null;
 
     /**
      * Constructor for EasyX509TrustManager.
      */
-    public EasyX509TrustManager( KeyStore keystore )
-        throws NoSuchAlgorithmException, KeyStoreException
-    {
+    public EasyX509TrustManager(KeyStore keystore)
+            throws NoSuchAlgorithmException, KeyStoreException {
         super();
-        TrustManagerFactory factory = TrustManagerFactory.getInstance( TrustManagerFactory.getDefaultAlgorithm() );
-        factory.init( keystore );
+        TrustManagerFactory factory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        factory.init(keystore);
         TrustManager[] trustmanagers = factory.getTrustManagers();
-        if ( trustmanagers.length == 0 )
-        {
-            throw new NoSuchAlgorithmException( "no trust manager found" );
+        if (trustmanagers.length == 0) {
+            throw new NoSuchAlgorithmException("no trust manager found");
         }
         this.standardTrustManager = (X509TrustManager) trustmanagers[0];
     }
 
     /**
-     * @see javax.net.ssl.X509TrustManager#checkClientTrusted(java.security.cert.X509Certificate[],String authType)
+     * @see javax.net.ssl.X509TrustManager#checkClientTrusted(java.security.cert.X509Certificate[], String authType)
      */
-    public void checkClientTrusted( X509Certificate[] certificates, String authType )
-        throws CertificateException
-    {
-        standardTrustManager.checkClientTrusted( certificates, authType );
+    public void checkClientTrusted(X509Certificate[] certificates, String authType)
+            throws CertificateException {
+        standardTrustManager.checkClientTrusted(certificates, authType);
     }
 
     /**
-     * @see javax.net.ssl.X509TrustManager#checkServerTrusted(java.security.cert.X509Certificate[],String authType)
+     * @see javax.net.ssl.X509TrustManager#checkServerTrusted(java.security.cert.X509Certificate[], String authType)
      */
-    public void checkServerTrusted( X509Certificate[] certificates, String authType )
-        throws CertificateException
-    {
-        if ( ( certificates != null ) && ( certificates.length == 1 ) )
-        {
+    public void checkServerTrusted(X509Certificate[] certificates, String authType)
+            throws CertificateException {
+        if ((certificates != null) && (certificates.length == 1)) {
             certificates[0].checkValidity();
-        }
-        else
-        {
-            standardTrustManager.checkServerTrusted( certificates, authType );
+        } else {
+            standardTrustManager.checkServerTrusted(certificates, authType);
         }
     }
 
     /**
      * @see javax.net.ssl.X509TrustManager#getAcceptedIssuers()
      */
-    public X509Certificate[] getAcceptedIssuers()
-    {
+    public X509Certificate[] getAcceptedIssuers() {
         return this.standardTrustManager.getAcceptedIssuers();
     }
 
