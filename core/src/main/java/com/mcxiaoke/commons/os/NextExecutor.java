d9f3523cfef05243c8d210971591a3ae95e7eea8
zhangxiaoke <zhangxiaoke@douban.com>
2014-04-08 10:33:01 +0800

    fix concurrent exception for next executor

--- core/src/main/java/com/mcxiaoke/commons/os/NextExecutor.java
+++ core/src/main/java/com/mcxiaoke/commons/os/NextExecutor.java
@@ -10,8 +10,8 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-import java.util.WeakHashMap;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -52,7 +52,7 @@ public final class NextExecutor {
     }
 
     private void ensureData() {
-        mDispatchers = new WeakHashMap<String, NextDispatcher>();
+        mDispatchers = new ConcurrentHashMap<String, NextDispatcher>();
     }
 
 
