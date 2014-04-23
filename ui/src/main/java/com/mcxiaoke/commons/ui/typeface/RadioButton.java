7ba0a82b14de4979be69ea5f37bc0c137e72da60
zhangxiaoke <zhangxiaoke@douban.com>
2014-04-23 17:28:16 +0800

    add typeface series views

---
+++ ui/src/main/java/com/mcxiaoke/commons/ui/typeface/RadioButton.java
@@ -0,0 +1,30 @@
+package com.mcxiaoke.commons.ui.typeface;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+
+public class RadioButton extends android.widget.RadioButton {
+    public RadioButton(Context context) {
+        super(context);
+    }
+
+    public RadioButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // return early for eclipse preview mode
+        if (isInEditMode()) return;
+
+        FontCache.getInstance().setFont(this, attrs);
+    }
+
+    public void setFont(String fontPath) {
+        FontCache.getInstance().setFont(this, fontPath);
+    }
+
+    public void setFont(int resId) {
+        String fontPath = getContext().getString(resId);
+        setFont(fontPath);
+    }
+}
