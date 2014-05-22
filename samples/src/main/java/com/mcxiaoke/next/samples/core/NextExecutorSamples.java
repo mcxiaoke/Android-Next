d0b65aa9893f7921a8d97804fcb5e3a7f266a7fa
mcxiaoke <mcxiaoke@gmail.com>
2014-05-22 16:03:32 +0800

    add next executor example

---
+++ samples/src/main/java/com/mcxiaoke/next/samples/core/NextExecutorSamples.java
@@ -0,0 +1,118 @@
+package com.mcxiaoke.next.samples.core;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import com.mcxiaoke.next.http.NextRequest;
+import com.mcxiaoke.next.os.NextCallable;
+import com.mcxiaoke.next.os.NextExecutor;
+import com.mcxiaoke.next.os.NextMessage;
+import com.mcxiaoke.next.os.TaskCallback;
+import com.mcxiaoke.next.samples.BaseActivity;
+import com.mcxiaoke.next.samples.R;
+import com.mcxiaoke.next.utils.StringUtils;
+
+/**
+ * User: mcxiaoke
+ * Date: 14-5-22
+ * Time: 15:13
+ */
+public class NextExecutorSamples extends BaseActivity {
+    public static final String TAG = NextExecutorSamples.class.getSimpleName();
+
+    @InjectView(R.id.input)
+    EditText mEditText;
+
+    @InjectView(R.id.button1)
+    Button mButton;
+
+    @InjectView(R.id.text1)
+    TextView mTextView;
+
+    @OnClick(R.id.button1)
+    public void onClick(View view) {
+        doRequest();
+    }
+
+    private boolean mRunning;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.act_next_executor);
+        ButterKnife.inject(this);
+        NextExecutor.getDefault().setDebug(true);
+    }
+
+    private void doRequest() {
+        if (mRunning) {
+            showToast("task is running already!");
+            return;
+        }
+        mRunning = true;
+
+        final String url = mEditText.getText().toString();
+        if (StringUtils.isEmpty(url)) {
+            showToast("url cannot be null.");
+            return;
+        }
+
+        final TaskCallback<String> callback = new TaskCallback<String>() {
+            @Override
+            public void onTaskSuccess(final String result, final NextMessage message) {
+                println("success, end http request.\n");
+                println("Response:\n");
+                println(result);
+                println("\n");
+                mRunning = false;
+                hideProgressIndicator();
+            }
+
+            @Override
+            public void onTaskFailure(final Throwable ex, final NextMessage message) {
+                println("failure, end http request.\n");
+                println("Error:\n");
+                println("" + Log.getStackTraceString(ex));
+                println("\n");
+                mRunning = false;
+                hideProgressIndicator();
+            }
+        };
+        final NextMessage message = new NextMessage(1001, 123, 456, true);
+        final NextCallable<String> callable = new NextCallable<String>(message) {
+            @Override
+            public String call() throws Exception {
+                final NextRequest request = NextRequest.get(url);
+                return request.asString();
+            }
+        };
+        clearText();
+        println("url:" + url);
+        println("Start http request...");
+        showProgressIndicator();
+        NextExecutor.getDefault().execute(callable, callback, this);
+    }
+
+    private void clearText() {
+        mTextView.setText(null);
+    }
+
+    private void println(CharSequence text) {
+        mTextView.append(text);
+        mTextView.append("\n");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        NextExecutor.getDefault().cancelAll(this);
+    }
+}
