18a8937c8dc10d2cd85fb326f26ee2591415da0d
mcxiaoke <mcxiaoke@gmail.com>
2015-03-24 12:23:47 +0800

    add new ListViewExtend, a refactoring version of EndlessListView

---
+++ ui/src/main/java/com/mcxiaoke/next/ui/list/AdapterExtend.java
@@ -0,0 +1,205 @@
+/***
+ Copyright (c) 2008-2009 CommonsWare, LLC
+ Portions (c) 2009 Google, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may
+ not use this file except in compliance with the License. You may obtain
+ a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package com.mcxiaoke.next.ui.list;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import com.mcxiaoke.next.ui.BuildConfig;
+import com.mcxiaoke.next.ui.internal.AdapterWrapper;
+import com.mcxiaoke.next.ui.list.ListViewExtend.OnRefreshListener;
+import com.mcxiaoke.next.ui.view.SimpleProgressView;
+
+/**
+ * User: mcxiaoke
+ * Date: 13-8-14
+ * Time: 下午9:32
+ */
+class AdapterExtend extends AdapterWrapper {
+    private static final String TAG = AdapterExtend.class.getSimpleName();
+    private static final boolean DEBUG = BuildConfig.DEBUG;
+
+    private Context mContext;
+    private boolean mEnableRefreshing;
+    private boolean mRefreshing;
+    private SimpleProgressView mFooter;
+    private OnRefreshListener mRefreshListener;
+
+    /**
+     * Constructor wrapping a supplied ListAdapter
+     */
+    public AdapterExtend(Context context, ListAdapter wrapped) {
+        super(wrapped);
+        mContext = context;
+        mEnableRefreshing = true;
+        mRefreshing = false;
+        mFooter = new SimpleProgressView(context);
+    }
+
+    public void setRefreshListener(OnRefreshListener listener) {
+        mRefreshListener = listener;
+    }
+
+    public boolean isRefreshing() {
+        return mRefreshing;
+    }
+
+    public boolean isEnableRefreshing() {
+        return mEnableRefreshing;
+    }
+
+    public void setEnableRefreshing(final boolean value) {
+        mEnableRefreshing = value;
+    }
+
+    public void setOnClickListener(OnClickListener listener) {
+        mFooter.setOnClickListener(listener);
+    }
+
+    /**
+     * How many items are in the data set represented by this
+     * Adapter.
+     */
+    @Override
+    public int getCount() {
+        if (isEnableRefreshing()) {
+            return super.getCount() + 1;
+        }
+        return (super.getCount());
+    }
+
+    /**
+     * Masks ViewType so the AdapterView replaces the
+     * "Pending" row when new data is loaded.
+     */
+    public int getItemViewType(int position) {
+        if (position == getWrappedAdapter().getCount() && isEnableRefreshing()) {
+            return (IGNORE_ITEM_VIEW_TYPE);
+        }
+
+        return (super.getItemViewType(position));
+    }
+
+    /**
+     * Masks ViewType so the AdapterView replaces the
+     * "Pending" row when new data is loaded.
+     *
+     * @see #getItemViewType(int)
+     */
+    public int getViewTypeCount() {
+        if (isEnableRefreshing()) {
+            return (super.getViewTypeCount() + 1);
+        }
+        return super.getViewTypeCount();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if (position >= super.getCount()) {
+            return (null);
+        }
+
+        return (super.getItem(position));
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return (false);
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        if (position >= super.getCount()) {
+            return (false);
+        }
+
+        return (super.isEnabled(position));
+    }
+
+    /**
+     * Get a View that displays the data at the specified
+     * position in the data set. In this case, if we are at
+     * the end of the list and we are still in append mode, we
+     * ask for a pending view and return it, plus kick off the
+     * background task to append more data to the wrapped
+     * adapter.
+     *
+     * @param position    Position of the item whose data we want
+     * @param convertView View to recycle, if not null
+     * @param parent      ViewGroup containing the returned View
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (position == super.getCount() && isEnableRefreshing()) {
+            return (mFooter);
+        }
+        return (super.getView(position, convertView, parent));
+    }
+
+    public void onStateChanged(boolean refreshing, boolean needNotify) {
+        boolean stateChanged = refreshing != mRefreshing;
+        mRefreshing = refreshing;
+        if (stateChanged && needNotify && mRefreshListener != null) {
+            mRefreshListener.onRefresh(null);
+        }
+    }
+
+    /**
+     * Getter method for the Context being held by the adapter
+     *
+     * @return Context
+     */
+    protected Context getContext() {
+        return (mContext);
+    }
+
+    public void setRefreshing(boolean needNotify) {
+        if (DEBUG) {
+            Log.v(TAG, "setRefreshing() isRefreshing=" + isRefreshing());
+        }
+        if (isRefreshing()) {
+            return;
+        }
+        onStateChanged(true, needNotify);
+        mFooter.showProgress();
+    }
+
+    public void showFooterEmpty() {
+        if (DEBUG) {
+            Log.v(TAG, "showFooterEmpty() isRefreshing=" + isRefreshing());
+        }
+        onStateChanged(false, true);
+        mFooter.showEmpty();
+    }
+
+    public void showFooterText(int resId) {
+        if (DEBUG) {
+            Log.v(TAG, "showFooterText() text=" + getContext().getString(resId));
+        }
+        showFooterText(getContext().getString(resId));
+    }
+
+    public void showFooterText(CharSequence text) {
+        if (DEBUG) {
+            Log.v(TAG, "showFooterText() text=" + text);
+        }
+        onStateChanged(false, true);
+        mFooter.showText(text);
+    }
+}
---
+++ ui/src/main/java/com/mcxiaoke/next/ui/list/ListViewExtend.java
@@ -0,0 +1,191 @@
+package com.mcxiaoke.next.ui.list;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import com.mcxiaoke.next.ui.BuildConfig;
+
+/**
+ * User: mcxiaoke
+ * Date: 13-8-14
+ * Time: 下午9:32
+ */
+public class ListViewExtend extends ListView implements AbsListView.OnScrollListener {
+    public static final String TAG = ListViewExtend.class.getSimpleName();
+    public static final boolean DEBUG = BuildConfig.DEBUG;
+
+    public static final int MODE_NONE = 0;
+    public static final int MODE_MANUAL = 1;
+    public static final int MODE_AUTO = 2;
+
+    public interface OnRefreshListener {
+        void onRefresh(ListViewExtend listView);
+    }
+
+    private AdapterExtend mEndlessAdapter;
+    private OnScrollListener mOnScrollListener;
+    private OnRefreshListener mOnRefreshListener;
+    private int mFirstVisibleItem;
+    private int mVisibleItemCount;
+    private int mTotalItemCount;
+    private int mRefreshMode = MODE_NONE;
+
+
+    public ListViewExtend(Context context) {
+        super(context);
+        setUp(context);
+    }
+
+    public ListViewExtend(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setUp(context);
+    }
+
+    public ListViewExtend(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setUp(context);
+    }
+
+    private void setUp(Context context) {
+        setFadingEdgeLength(0);
+        super.setOnScrollListener(this);
+    }
+
+    @Override
+    public void setOnScrollListener(OnScrollListener listener) {
+        mOnScrollListener = listener;
+    }
+
+    @Override
+    public void setAdapter(ListAdapter adapter) {
+        mEndlessAdapter = new AdapterExtend(getContext(), adapter);
+        mEndlessAdapter.setRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(final ListViewExtend listView) {
+                notifyRefresh();
+            }
+        });
+        super.setAdapter(mEndlessAdapter);
+    }
+
+    public void setOnRefreshListener(OnRefreshListener listener) {
+        mOnRefreshListener = listener;
+    }
+
+    public void setRefreshMode(int mode) {
+        if (DEBUG) {
+            Log.v(TAG, "setRefreshMode() mode=" + mode);
+        }
+        mRefreshMode = mode;
+        checkManualRefresh();
+    }
+
+    public int getRefreshMode() {
+        return mRefreshMode;
+    }
+
+    /**
+     * 只显示进度圆圈，但是不回调onRefresh
+     */
+    public void showFooterRefreshing() {
+        mEndlessAdapter.setRefreshing(false);
+    }
+
+    public void showFooterEmpty() {
+        mEndlessAdapter.showFooterEmpty();
+    }
+
+    public void showFooterText(int resId) {
+        mEndlessAdapter.showFooterText(resId);
+    }
+
+    public void showFooterText(CharSequence text) {
+        mEndlessAdapter.showFooterText(text);
+    }
+
+    public boolean isRefreshing() {
+        return mEndlessAdapter.isRefreshing();
+    }
+
+    private void notifyRefresh() {
+        if (mOnRefreshListener != null) {
+            mOnRefreshListener.onRefresh(this);
+        }
+    }
+
+    private void checkManualRefresh() {
+        if (mEndlessAdapter == null) {
+            return;
+        }
+        if (mRefreshMode == MODE_MANUAL) {
+            mEndlessAdapter.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mEndlessAdapter.setRefreshing(true);
+                }
+            });
+        } else {
+            mEndlessAdapter.setOnClickListener(null);
+        }
+    }
+
+    private void checkAutoRefresh() {
+        if (DEBUG) {
+            Log.v(TAG, "checkRefresh() getRefreshMode=" + getRefreshMode()
+                    + " isRefreshing=" + isRefreshing());
+            Log.v(TAG, "checkRefresh() mFirstVisibleItem=" + mFirstVisibleItem
+                    + " mVisibleItemCount=" + mVisibleItemCount);
+            Log.v(TAG, "checkRefresh() mTotalItemCount=" + mTotalItemCount);
+        }
+        if (mRefreshMode != MODE_AUTO) {
+            return;
+        }
+        if (isRefreshing()) {
+            return;
+        }
+        if (mTotalItemCount == 0 || mVisibleItemCount >= mTotalItemCount) {
+            return;
+        }
+        if (mFirstVisibleItem + mVisibleItemCount >= mTotalItemCount) {
+            mEndlessAdapter.setRefreshing(true);
+        }
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        if (mOnScrollListener != null) {
+            mOnScrollListener.onScrollStateChanged(view, scrollState);
+        }
+        if (DEBUG) {
+            Log.v(TAG, "onScrollStateChanged() scrollState=" + scrollState);
+        }
+        if (SCROLL_STATE_IDLE == scrollState) {
+            checkAutoRefresh();
+        }
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        if (mOnScrollListener != null) {
+            mOnScrollListener.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount);
+        }
+        mFirstVisibleItem = firstVisibleItem;
+        mVisibleItemCount = visibleItemCount;
+        mTotalItemCount = totalItemCount;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return super.onTouchEvent(ev);
+    }
+}
