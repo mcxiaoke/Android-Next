03c2ea6a5b7ae798a7c995dfe312d217a18bf45f
mcxiaoke <mcxiaoke@gmail.com>
2015-07-31 13:23:20 +0800

    minor tweak samples

--- samples/src/main/java/com/mcxiaoke/next/samples/Samples.java
+++ samples/src/main/java/com/mcxiaoke/next/samples/Samples.java
@@ -3,6 +3,7 @@ package com.mcxiaoke.next.samples;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
