b61f40073fbb618483f3fc0bf70247d0346ae499
mcxiaoke <mcxiaoke@live.com>
2017-04-12 18:21:19 +0800

    add util class KeyValue

---
+++ http/src/main/java/com/mcxiaoke/next/http/KeyValue.java
@@ -0,0 +1,21 @@
+package com.mcxiaoke.next.http;
+
+import android.util.Pair;
+
+/**
+ * User: mcxiaoke
+ * Date: 2017/4/12
+ * Time: 17:54
+ */
+
+public class KeyValue extends Pair<String, String> {
+
+    public static KeyValue of(final String key, final String value) {
+        return new KeyValue(key, value);
+    }
+
+    public KeyValue(final String key, final String value) {
+        super(key, value);
+    }
+
+}
